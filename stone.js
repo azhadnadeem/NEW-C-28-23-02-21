4d810e1479351763fa4ba21eeb3724306bcc1d29
azhadnadeem <74958830+azhadnadeem@users.noreply.github.com>
2021-02-23 01:27:37 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,24 @@
+class Stone{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:0.5,
+            friction:1,
+            density:0.5
+        }
+        this.body=Bodies.rectangle(x,y,50,50,options);
+        this.width=50;
+        this.height=50;
+        this.image=loadImage("images/stone.png")
+        World.add(world,this.body);
+    }
+    display(){
+        var angle=this.body.angle;
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,50,50);
+        pop();
+    }
+}
